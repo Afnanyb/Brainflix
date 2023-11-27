e5cd1677ca5da8bd5b5f88c49821334d6b425822
Afnan <afnanyb@gmail.com>
2023-11-26 17:34:06 -0800

    commentlist added , using map

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Header from "./components/Header/Header";
 import videos from "./Data/video-details.json";
 import MainVideo from "./components/MainVideo/MainVideo";
 import CommentForm from "./components/CommentForm/CommentForm";
+import CommentList from "./components/CommentList/CommentList";
 import VideoList from "./components/VideoList/VideoList";
 
 function App() {
-  const [index, setIndex] = useState(0);
-  const selectedVideo = videos[index];
+  const firstVideo = videos[0];
+  const [id, setId] = useState(firstVideo.id);
+
+  const selectedVideo = videos.find((video) => video.id === id);
 
   console.log(selectedVideo);
   return (
@@ -17,8 +20,8 @@ function App() {
       <MainVideo video={selectedVideo} />
 
       <CommentForm />
-      {/* <CommentList /> */}
-      <VideoList setSelectedVideo={setIndex} />
+      <CommentList comments={selectedVideo.comments} />
+      <VideoList setSelectedVideo={setId} selectedVideo={selectedVideo} />
     </>
   );
 }
--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,27 +1,29 @@
 import videos from "../../Data/video-details.json";
 import "./VideoList.scss";
-function VideoList({ setSelectedVideo }) {
+function VideoList({ setSelectedVideo, selectedVideo }) {
   return (
     <section className="video-list">
       <p className="video-list__title">NEXT VIDEO</p>
-      {videos.map((video, index) => {
-        return (
-          <section
-            key={video.id}
-            className="next-video"
-            onClick={() => setSelectedVideo(index)}
-          >
-            <video
-              className="next-video__thumbnail"
-              poster={video.image}
-            ></video>
-            <div className="next-video__details">
-              <h3 className="next-video__title"> {video.title}</h3>
-              <p className="next-video__channel"> {video.channel}</p>
-            </div>
-          </section>
-        );
-      })}
+      {videos
+        .filter((video) => video.id !== selectedVideo.id)
+        .map((video, index) => {
+          return (
+            <section
+              key={video.id}
+              className="next-video"
+              onClick={() => setSelectedVideo(video.id)}
+            >
+              <video
+                className="next-video__thumbnail"
+                poster={video.image}
+              ></video>
+              <div className="next-video__details">
+                <h3 className="next-video__title"> {video.title}</h3>
+                <p className="next-video__channel"> {video.channel}</p>
+              </div>
+            </section>
+          );
+        })}
     </section>
   );
 }
