f059e72fd57004e17649a05c41409b2c25a3663e
Afnan <afnanyb@gmail.com>
2023-11-28 09:25:37 -0800

    fixing header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,24 +6,20 @@ import Image from "../../assets/Images/Mohan-muruge.jpg";
 function Header() {
   return (
     <>
-      <header className="header">
-        <div className="header__logo">
-          <img src={Logo} alt="Website logo" className="header__logo--img" />
+      <header className="container header">
+        <img className="header__logo" src={Logo} />
+
+        <div className="header__row">
+          <input className="header__search" placeholder="Search" type="text" />
+          <img className="header__avatar header__avatar--mobile" src={Image} />
         </div>
-        <form className="header__search">
-          <input
-            type="search"
-            placeholder="Search"
-            className="header__search--box"
-          ></input>
-          <img src={Image} alt="Avatar" className="header__avatar" />
-        </form>
-        <div className="header__upload">
-          <button className="header__button button">
-            <img src={Icon} />
-            UPLOAD
-          </button>
-        </div>
+
+        <button className="header__button">
+          <img src={Icon} />
+          Upload
+        </button>
+
+        <img className="header__avatar" src={Image} />
       </header>
     </>
   );
