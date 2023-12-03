2e62b53727a0e5c9a391a3078c681433adb2c2c1
Alex Wilmer <alex.wilmer@zensurance.com>
2023-12-03 16:47:49 -0700

    added fancy mouse moving colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import MainVideoPage from "./pages/MainVideoPage/MainVideoPage";
 import Upload from "./pages/Upload/Upload";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  window.onmousemove = (event) => {
+    document.body.style.backgroundColor = `hsl(${event.clientX / 3}, ${
+      event.clientY / 3
+    }%, 50%)`;
+  };
+
   return (
     <>
       <BrowserRouter>
